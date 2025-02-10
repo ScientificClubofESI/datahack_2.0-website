5c5f8fd66645143b5603949859224ac0a871ded8
yasmiinie <ly_zaidi@esi.dz>
2025-02-10 20:01:21 +0100

    adding datahack logo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
